0b0800e13c0dc677f916688638c92e6325230190
Shubham Ghadge <shubhamghadge784@gmail.com>
2024-02-06 22:21:36 +0530

    first commit

---
+++ jdbc/src/jdbc/Demo_4.java
@@ -0,0 +1,34 @@
+package jdbc;
+
+import java.sql.*;
+import java.util.*;
+
+public class Demo_4 {
+
+	public static void main(String[] args) {
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/info?autoReconnect=true&useSSL=false", "root", "Satara@123");
+			Scanner sc = new Scanner(System.in);
+			System.out.println("Enter name");
+			String nm = sc.next();
+			
+			PreparedStatement pst = con.prepareStatement("SELECT * FROM student WHERE Name = ?");
+            pst.setString(1, nm);
+            ResultSet rs = pst.executeQuery();
+            if(rs.next()) {
+            	System.out.println(rs.getInt(1)+""+rs.getString(2)+""+rs.getInt(3));
+            }else {
+            	System.out.println("Record not found");
+            }
+            rs.close();
+            con.close();
+			
+		}catch(Exception e){
+			e.printStackTrace();
+			
+		}
+
+	}
+
+}
